f6f9af0b1779c5ef59d7dcd8dd67b407136bbef2
Abir Santra <santraabir8@gmail.com>
2023-01-11 20:04:42 +0530

    removed unused icon

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -2,40 +2,39 @@ import React from "react";
 import "./form.css";
 import { useState } from "react";
 import PropTypes from "prop-types";
-import searchIcon from "../../img/search-solid.svg";
 
 const Form = ({ submitSearch }) => {
-    const [location, setLocation] = useState("");
+	const [location, setLocation] = useState("");
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (location === "" || !location) return;
-        // console.log(location);
-        submitSearch(location);
-    };
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (location === "" || !location) return;
+		// console.log(location);
+		submitSearch(location);
+	};
 
-    return (
-        <form className="search-form" onSubmit={onSubmit}>
-            <input
-                type="text"
-                aria-label="location"
-                className="form-input"
-                placeholder="Search City"
-                value={location}
-                onChange={(e) => {
-                    setLocation(e.target.value);
-                }}
-                required
-            />
-            <button type="submit" className="form-button" onClick={onSubmit}>
-                <i className="fas fa-search"></i>
-            </button>
-        </form>
-    );
+	return (
+		<form className="search-form" onSubmit={onSubmit}>
+			<input
+				type="text"
+				aria-label="location"
+				className="form-input"
+				placeholder="Search City"
+				value={location}
+				onChange={(e) => {
+					setLocation(e.target.value);
+				}}
+				required
+			/>
+			<button type="submit" className="form-button" onClick={onSubmit}>
+				<i className="fas fa-search"></i>
+			</button>
+		</form>
+	);
 };
 
 Form.propTypes = {
-    submitSearch: PropTypes.func.isRequired,
+	submitSearch: PropTypes.func.isRequired,
 };
 
 export default Form;
