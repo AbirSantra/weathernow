f012ef3b21e94bb4089656792e8cc7dbfc84f91d
Abir Santra <santraabir8@gmail.com>
2023-01-11 20:03:41 +0530

    moved error message to middle of header

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -8,38 +8,32 @@ import Forecast from "../Forecast/Forecast";
 import useForecast from "../../hooks/useForecast";
 
 const Main = () => {
-    const { isError, isLoading, forecast, submitRequest, background } =
-        useForecast();
+	const { isError, isLoading, forecast, submitRequest } = useForecast();
 
-    const onSubmit = (value) => {
-        submitRequest(value);
-    };
+	const onSubmit = (value) => {
+		submitRequest(value);
+	};
 
-    console.log(background);
-    return (
-        <>
-            <div className="main">
-                <div className="backImg">
-                    {/* <img src={background} alt="" /> */}
-                </div>
-                <div className="main-info">
-                    <div className="header-content">
-                        <Header></Header>
-                        {/* Form */}
-                        {<Form submitSearch={onSubmit} />}
-                        {/* Error */}
-                        {isError && <Error message={isError} />}
-                    </div>
-                    {/* Loader */}
-                    {isLoading && <Loader></Loader>}
-                    {/* Forecast */}
-                    {forecast && !isLoading && (
-                        <Forecast forecast={forecast}></Forecast>
-                    )}
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="main">
+				<div className="backImg">{/* <img src={background} alt="" /> */}</div>
+				<div className="main-info">
+					<div className="header-content">
+						<Header></Header>
+						{/* Error */}
+						{isError && <Error message={isError} />}
+						{/* Form */}
+						{<Form submitSearch={onSubmit} />}
+					</div>
+					{/* Loader */}
+					{isLoading && <Loader></Loader>}
+					{/* Forecast */}
+					{forecast && !isLoading && <Forecast forecast={forecast}></Forecast>}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Main;
